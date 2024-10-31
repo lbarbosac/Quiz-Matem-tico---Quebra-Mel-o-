3a93738ce5552b0ef684aeb9d13b6123ab2c14fe
Lucas Barbosa <lucas2304barbosa@gmail.com>
2024-10-31 08:21:30 -0300

    .

--- paginicial/primeiraquestao/primeiraquestao.js
+++ paginicial/primeiraquestao/primeiraquestao.js
@@ -1,12 +1,1 @@
 
-const pergunta = [
-    {
-        questions: "Qual ano o Grêmio ganhou sua primeira Libertadores?",
-        alternativa: [
-            { text: "1983", correct: true},
-            { text: "1995", correct: false},
-            { text: "2017", correct: false},
-            { text: "1993", correct: false}
-        ]
-    }
-]
